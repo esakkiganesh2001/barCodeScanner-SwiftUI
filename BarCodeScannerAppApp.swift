8b903ddaa34c146df7400647577d03c08fd52c07
Esakki Ganesh <121609386+esakkiganesh2001@users.noreply.github.com>
2024-07-19 21:20:53 +0530

    Add files via upload

---
+++ BarCodeScannerAppApp.swift
@@ -0,0 +1,16 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by Esakki-Ios on 13/07/24.
+//
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
---
+++ BarcodeScannerViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  BarCodeScannerViewModel.swift
+//  BarCodeScanner
+//
+//  Created by Esakki-Ios on 13/07/24.
+//
+
+import SwiftUI
+
+final class BarcodeScannerViewModel: ObservableObject {
+    
+    @Published var scannedCode = ""
+    @Published var alertItem: AlertItem?
+    
+    var statusText: String {
+        scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode
+    }
+    
+    var statusTextColor: Color {
+        scannedCode.isEmpty ? .red : .green
+    }
+}
